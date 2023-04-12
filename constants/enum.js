8aa4ee0b83d14a4cbd3768a98ef4e4da2c7cf82a
kiranguyen2000 <98039366+kiranguyen2000@users.noreply.github.com>
2023-04-12 09:43:30 +0700

    feat: create-database (#1)

---
+++ constants/enum.js
@@ -0,0 +1,23 @@
+export const RequestSTT = {
+  PENDING: 'Pending',
+  APPROVE: 'Approve',
+  REJECT: 'Reject',
+  CANCEL: 'Cancel',
+  CHANGE_REQUEST: 'Change Request',
+  UPDATE: 'Update',
+};
+
+export const Role = {
+  STAFF: 'Staff',
+  ADMIN: 'Admin',
+  MANAGER: 'Manager',
+};
+
+export const TypeHistory = {
+  CREATE: 'CREATE',
+  UPDATE: 'UPDATE',
+  APPROVE: 'APPROVE',
+  REJECT: 'REJECT',
+  CHANGE_REQUEST: 'CHANGE_REQUEST',
+  CANCEL: 'CANCEL',
+};
---
+++ models/History.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+import { TypeHistory } from '../constants/enum';
+const Schema = mongoose.Schema;
+
+const HistorySchema = new Schema(
+  {
+    idlogoff: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'requestlogoffs',
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+    },
+
+    masters: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    ],
+
+    approval: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+      },
+    ],
+
+    logofffrom: {
+      type: Date,
+    },
+
+    logoffto: {
+      type: Date,
+    },
+
+    reason: {
+      type: String,
+    },
+
+    comment: {
+      type: String,
+    },
+
+    typelog: {
+      type: String,
+      enum: Object.values(TypeHistory),
+    },
+
+    contentlog: {
+      type: String,
+    },
+
+    quantity: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const History = mongoose.model('histories', HistorySchema);
